9874014596bd8dba040247871c638093766ae789
Florian Spieß <business@minnced.club>
2023-07-23 18:53:05 +0200

    Use setInterval and loop to update timers

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showTab(tabName) {
     .classList.add("active");
 }
 
-// Create number elements
+// Create day elements
 const numbersContent = document.getElementById("numbersContent");
 for (let i = 1; i <= 31; i++) {
   const numberContainer = document.createElement("div");
@@ -51,7 +51,7 @@ const months = [
 ];
 for (const month of months) {
   const monthContainer = document.createElement("div");
-  monthContainer.className = "months-container";
+  monthContainer.className = "number-container";
   monthsContent.appendChild(monthContainer);
 
   const monthDiv = document.createElement("div");
@@ -81,7 +81,7 @@ const Smonts = {
 };
 for (const season of seasons) {
   const seasonContainer = document.createElement("div");
-  seasonContainer.className = "seasons-container";
+  seasonContainer.className = "number-container";
   seasonsContent.appendChild(seasonContainer);
 
   const seasonDiv = document.createElement("div");
@@ -101,62 +101,60 @@ for (const season of seasons) {
   seasonContainer.appendChild(bottomDiv);
 }
 
+// Add index id to each container to simplify updating the state
+const numberContainers = document.querySelectorAll(".number-container");
+numberContainers.forEach((container, index) => {
+  container.id = `container-${index}`;
+  container.addEventListener('click', handleClick);
+});
+
+// Keep the state of each tile
+const timers = Array(numberContainers.length).fill(0);
+
+// Update all the timers every second
+// This is better than doing an interval for each timer, because they might run out of sequence which can cause drift
+// Timers might also be delayed if the event loop can't run it in time, by using the timestamp this is not a problem.
+setInterval(() => {
+  const time = new Date() / 1000;
+  for (let i = 0; i < timers.length; i++) {
+    const container = numberContainers[i];
+    const bottomElement = container.querySelector(".bottom");
+    const backgroundEl = container.querySelector(".boxborder");
+    
+    if (timers[i] > time) {
+      // If not ready, display the current timer
+      const remaining = timers[i] - time;
+      const minutes = Math.floor(remaining / 60);
+      const seconds = Math.floor(remaining % 60);
+  
+      bottomElement.innerText = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+      container.removeEventListener('click', handleClick);
+    } else if (bottomElement.innerText !== "Ready") {
+      // Otherwise, remove the styles and display "Ready"
+      bottomElement.innerText = "Ready";
+      removeInlineStyles(bottomElement);
+      removeInlineStyles(backgroundEl);
+      container.addEventListener('click', handleClick);
+    }
+  }
+}, 1000);
+
 // Function to handle the countdown logic
 function startCountdown(container) {
+  // Adding the styles to disable the click and show the timer
   const bottomElement = container.querySelector(".bottom");
   const backgroundEl = container.querySelector(".boxborder");
   bottomElement.style.backgroundColor = "grey";
   backgroundEl.style.backgroundColor = "grey";
   backgroundEl.style.color = "#939393";
-
-  let count = 300; // 5 minutes in seconds (5 minutes * 60 seconds)
-  const countdownInterval = setInterval(() => {
-    const minutes = Math.floor(count / 60);
-    const seconds = count % 60;
-    bottomElement.innerText = `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
-    count--;
-
-    if (count < 0) {
-      clearInterval(countdownInterval);
-      bottomElement.innerText = "Ready"; // Change the text to "Ready" after the countdown is complete
-
-      // Remove the inline styles
-      removeInlineStyles(bottomElement);
-      removeInlineStyles(backgroundEl);
-
-      // Re-add the click event listener
-      container.addEventListener("click", handleClick);
-    }
-  }, 1000); // Update the countdown every second
-
-  // Remove the click event listener to prevent multiple countdowns
-  container.removeEventListener("click", handleClick);
+  // Update the timer expiration to 5 minutes from now, this will be handled by the setInterval handler above
+  timers[container.id.split("-")[1]] = new Date() / 1000 + 60 * 5;
 }
 
 function removeInlineStyles(element) {
   element.removeAttribute("style");
 }
 
-const numberContainers = document.querySelectorAll(".number-container");
-
-numberContainers.forEach((container) => {
-  container.addEventListener("click", handleClick);
-});
-
 function handleClick() {
   startCountdown(this);
 }
-
-const monthContainers = document.querySelectorAll(".months-container");
-
-monthContainers.forEach((container) => {
-  container.addEventListener("click", handleClick);
-});
-
-const seasonContainers = document.querySelectorAll(".seasons-container");
-
-seasonContainers.forEach((container) => {
-  container.addEventListener("click", handleClick);
-});
